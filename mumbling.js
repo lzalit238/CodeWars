9b65698c9f14505b749435f24d65e2dd3a56ebcb
lzalit238 <lzalit238@west-mec.org>
2021-11-05 13:40:55 -0700

    2 More CodeWars

---
+++ mumbling.js
@@ -0,0 +1,7 @@
+function accum(s) {
+  for (let i=0; i < s; i++) {
+    
+  }
+}
+
+accum("NyffsGeyylB")// "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb")
--- spinningWords.js
+++ spinningWords.js
@@ -10,14 +10,33 @@ function spinWords(string){
 spinWords("Welcome");
 
 function spinWords(string){
+  const myArray = string.split(' ');
+  myArray.map(str,i)
   if(string > 4 ) {
-      let splitting = string.split("");
-      let reversing = string.reverse();
-      let joining = string.join("");
-      return joining; 
-  } else {
+      let splitString = string.split("");
+      let reverseString = splitString.reverse();
+      let joinString = reverseString.join("");
+      return joinString; 
+  } else if(string <= 4) {
     return string;
   }
 }
 
 spinWords("Welcome");
+
+
+function spinWords(string) {
+  return string.split("").reverse().join("");
+}
+
+spinWords("Hello");
+
+
+//spins whole sentence
+function spinWords(string) {
+  if (string >= 5) {
+      return string.split("").reverse().join("");
+  } else {
+    return string;
+  }
+}
